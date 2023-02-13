9b81cc1b3214f7469873714d292436012e3bada0
choisunghwan <dute7570@naver.com>
2023-02-13 15:50:43 +0900

    정상작동

--- src/components/util/login-util.js
+++ src/components/util/login-util.js
@@ -3,7 +3,8 @@
 export const getToken = () => localStorage.getItem('ACCESS_TOKEN');
 
 // 로그인 유저의 이름을 반환하는 함수
-// export const getUsername = () => localStorage.getItem('LOGIN_USERNAME');
+export const getUsername = () => localStorage.getItem('LOGIN_USERNAME');
+export const getUserId = () => localStorage.getItem('LOGIN_USERID');
 
 // 로그인 상태인지 검증해주는 함수
 export const isLogin = () => !!getToken();
